ccfaa132f255c9c5940eb416542168219ec2717b
Andrea Di Persio <git@andreadipersio.com>
2023-10-16 23:40:35 +0200

    Consistent support of eVil keybindings

--- my-packages.el
+++ my-packages.el
@@ -339,4 +339,13 @@
 ;;
 (use-package evil
   :ensure t
-  :config (evil-mode 1))
+  :init
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
